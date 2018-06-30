46ab89f91c5febe63f6747d5e9e7b9484054e743
loegare <shnipes@gmail.com>
2018-06-30 01:26:44 -0400

    Get timezone into python
    get timezone into python to get unix time to imput into googlemaps api

--- datePicker.py
+++ datePicker.py
@@ -7,22 +7,37 @@ Created on Sat Jun 30 00:07:49 2018
 Open simple webpage and get user entered day of week and time
 """
 
-from flask import Flask, request, render_template,redirect,url_for
+from flask import Flask, request, render_template,redirect,url_for,render_template_string
 #from flask_login import login_required
 import pandas as pd
 import numpy as np
+import json
+from datetime import datetime,timedelta,timezone
 
 app = Flask(__name__)
 application = app
-@app.route('/',methods=['GET','POST'])
+@app.route('/Home',methods=['GET','POST'])
 def home():
-    print(1)
+    curDate = datetime.now(timezone.utc)
+    deptDatetime = curDate-timedelta(minutes=curDate.minute)
+    deptDatetime = deptDatetime-timedelta(hours=deptDatetime.hour)
+    deptDatetime = deptDatetime-timedelta(days=deptDatetime.weekday())
+    deptDatetime = deptDatetime+timedelta(days=7)
     if request.method =='GET':
         return render_template('Home.html')
     else:
         time = request.form['DeptTime']
+        print(time)
         day = request.form['DoW']
-        print(day,time)
+        print(day)
+#        tzOffset = request.form['test']
+        data = request.get_json()
+        print(data)
+#        deptDatetime = deptDatetime + timedelta(days=day)
+#        deptDatetime = deptDatetime + timedelta(hours=time)
+#        print(tzOffset)
         return render_template('Home.html')
+    
+
 if __name__ == "__main__":
     app.run()
